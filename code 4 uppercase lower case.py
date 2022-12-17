4a724de8a2b88393f030acd746932d9a549f7fe7
aman2004sp <119393298+aman2004sp@users.noreply.github.com>
2022-12-17 11:00:56 +0530

    Create code 4 uppercase lower case.py

---
+++ code 4 uppercase lower case.py
@@ -0,0 +1,10 @@
+
+char = str(input("enter a string:"))
+if char.islower():
+  print(char,"is in lower case")
+
+elif char.isupper():
+  print(char,"is in upper case")
+
+else:
+  print("enter any single letter only!!!")          
