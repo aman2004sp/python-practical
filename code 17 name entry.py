07a6a9efa32759b88dea163ae28d509612e40738
aman2004sp <119393298+aman2004sp@users.noreply.github.com>
2023-02-16 19:09:12 +0530

    Update code 17 name entry.py

--- code 17 name entry.py
+++ code 17 name entry.py
@@ -1,6 +1,20 @@
-name = input("enter your name:_")
-if name.isalpha():
-    print("your name {0} has been accepted".format(name))
-    
-else:
-    print("entered name must be in alphabet")
+def checking_name(name):
+    if any(char.isdigit() or not char.isalpha() for char in name):
+        raise ValueError("Invalid name. Please enter only alphabets.")
+    else:
+        print("Valid name entered.")
+        
+l = True
+while l == True:
+      
+    try:
+        name = input("Enter your name: ")
+        checking_name(name)
+    except ValueError as ve:
+        print(ve)
+
+    l = input("Do you want to run the program again? (y/n): ")
+    if l == "y":
+        l = bool(1)
+    else:
+        l = bool(0)
